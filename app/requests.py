8af21f6ed2ce4b50f96ae9b279d81130c309ddcd
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-01 02:20:25 +0300

    This commit calls the app.config

--- app/requests.py
+++ app/requests.py
@@ -3,4 +3,15 @@ from .models import Article, Category, Source , Headlines
 
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
+# Getting source url
+source_url= None
+# Getting source url
+cat_url= None
+
+def configure_request(app):
+    global api_key, source_url, cat_url
+    api_key = app.config['NEWS_API_KEY']
+    source_url= app.config['NEWS_API_SOURCE_URL']
+    cat_url=app.config['CAT_API_URL']
+
