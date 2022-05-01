5cc933efe2dc67207ef0fc5c7d1131fb6df597e8
Diana Kariuki <dianakariuki842@gmail.com>
2022-05-02 01:57:06 +0300

    This commit solves bugs on bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,21 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
+
+
 bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-    app = Flask(__name__)
+    app = Flask(__name__,instance_relative_config = True)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-
+    
+    
 # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
